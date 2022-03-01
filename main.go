3235d4b1384fe237a09b0ff93ad99db820907ce4
Shivansh Vij <shivanshvij@loopholelabs.io>
2022-03-01 16:22:40 -0500

    Using authorization header

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"github.com/OpenCal-FYDP/Authorization"
 	"github.com/OpenCal-FYDP/PreferenceManagement/internal/service"
 	"github.com/OpenCal-FYDP/PreferenceManagement/internal/storage"
 	"github.com/OpenCal-FYDP/PreferenceManagement/rpc"
+	"github.com/twitchtv/twirp"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
+var authorizeMethods = []string{
+	"GetUserProfile",
+	"GetAvailability",
+	"GetBooking",
+	"SetUserProfile",
+	"SetAvailability",
+}
+
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
 func main() {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = randomString(128)
+		fmt.Printf("Randomly Generated Secret: %s\n", secret)
+	}
 	svc := service.New(storage.New())
-	server := rpc.NewPreferenceManagementServiceServer(svc)
-	log.Fatal(http.ListenAndServe(":8080", server))
+	server := rpc.NewPreferenceManagementServiceServer(svc, twirp.WithServerInterceptors(Authorization.NewAuthorizationInterceptor([]byte(secret), authorizeMethods...)))
+	jwtServer := Authorization.WithJWT(server)
+	log.Fatal(http.ListenAndServe(":8080", jwtServer))
 }
